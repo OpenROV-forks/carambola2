928a437bab95eda7401ac173634220cf29179803
Gilbert Montague <gilbert.i.montague@gmail.com>
2017-02-16 17:40:12 -0800

    Special script to build our topside image

---
+++ build_topside.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+help(){
+    echo "Usage: $0 [board-name]"
+    echo "valid board names: carambola2 centipede lima rambutan topside"
+    exit
+}
+
+if [[ $# -lt 1 ]]
+then
+    config=config_minimal
+else
+    case $1 in
+        rambutan)
+            config=config_rambutan_minimal
+            ;;
+        carambola2|centipede|lima)
+            config=config_minimal
+            ;;
+	topside)
+	    config=config_topside
+	    ;;
+        help|--help|-h|*)
+            help
+            ;;
+    esac
+fi
+
+echo "Building image using config: '$config'"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
+cp .config .config_save`date +%F_%T.%N`
+cp $config .config
+make defconfig
+make -j4 IGNORE_ERRORS=m
