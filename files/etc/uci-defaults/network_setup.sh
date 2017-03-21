d9803ba6bbcc3b3cab1b29167e1f5acd2c99e88c
Gilbert Montague <gilbert.i.montague@gmail.com>
2017-03-20 19:15:23 -0700

    Added multicast support

--- files/etc/uci-defaults/network_setup.sh
+++ files/etc/uci-defaults/network_setup.sh
@@ -4,7 +4,17 @@ IP_ADDR="10.1.1.1"
 
 # Set the default IP address for the Carambola
 uci set network.lan.ipaddr="$IP_ADDR"
+uci set network.lan.igmp_snooping="1"
 uci commit network
+
+# Firewall setup
+uci add firewall rule
+uci set firewall.@rule[-1].name="Allow-IGMP"
+uci set firewall.@rule[-1].src="wan"
+uci set firewall.@rule[-1].proto="igmp"
+uci set firewall.@rule[-1].family="ipv4"
+uci set firewall.@rule[-1].target="ACCEPT"
+uci commit firewall
 
 # Let uci know we are good to go. This will delete the script
 exit 0
